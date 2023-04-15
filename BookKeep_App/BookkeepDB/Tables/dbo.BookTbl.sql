6df88d29d972b605adc941e52de6e3f64f2e01d5
Vachno <130864880+Vachn0@users.noreply.github.com>
2023-04-15 23:12:05 +0400

    Add files via upload

---
+++ BookKeep_App/BookkeepDB/Tables/dbo.BookTbl.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[BookTbl] (
+    [BookID]    INT        NOT NULL,
+    [BookName]  NCHAR (30) NULL,
+    [Author]    NCHAR (15) NULL,
+    [Publisher] NCHAR (15) NULL,
+    [Cover]     IMAGE      NULL,
+    [Available] FLOAT (53) NULL,
+    CONSTRAINT [PK_BookTbl] PRIMARY KEY CLUSTERED ([BookID] ASC),
+    CONSTRAINT [FK_BookTbl_BookTbl] FOREIGN KEY ([BookID]) REFERENCES [dbo].[RentTbl] ([RenterID])
+);
+
---
+++ BookKeep_App/BookkeepDB/Tables/dbo.RentTbl.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RentTbl] (
+    [RenterID]    INT  NOT NULL,
+    [RentDate]    DATE NULL,
+    [RentingDays] INT  NULL,
+    [ReturnDate]  DATE NULL,
+    CONSTRAINT [PK_RentTbl] PRIMARY KEY CLUSTERED ([RenterID] ASC)
+);
+
